954156f0652755379e6fa9cdc26e02ab7c9e7f2e
Paulus H.J. Schulinck <PaulskPt@users.noreply.github.com>
2025-07-23 00:23:09 +0100

    Update secrets.h
    Added an item to secret.h

--- src/Publisher/Feather_ESP32_S3_TFT_MQTT_multi_topic/secrets.h
+++ src/Publisher/Feather_ESP32_S3_TFT_MQTT_multi_topic/secrets.h
@@ -1,11 +1,12 @@
-#define SECRET_SSID "<Your_WiFi_SSID_here>"
-#define SECRET_PASS "<Your_WiFi_Password_here>"
+#define SECRET_SSID "Vodafone-8D96F1"
+#define SECRET_PASS "z2pTqpFMMX6jXdv3"
 #define SECRET_TIMEZONE_OFFSET "1" // Europe/Lisbon
 // #define TIMEZONE_OFFSET "-4" // America/New_York
+#define SECRET_USE_BROKER_LOCAL "1"  // We usa a local MQTT broker
 #define SECRET_NTP_SERVER1 "1.pt.pool.ntp.org"
 #define SECRET_MQTT_BROKER "5.196.78.28" // test.mosquitto.org"
-#define SECRET_MQTT_BROKER_LOCAL1 "192.168._.___"  // Local mosquitto broker app on PC Paul5
-#define SECRET_MQTT_BROKER_LOCAL2 "192.168._.___"  // Local mosquitto broker app on RPi CM5
+#define SECRET_MQTT_BROKER_LOCAL1 "192.168.1.96"  // Local mosquitto broker app on PC Paul5
+#define SECRET_MQTT_BROKER_LOCAL2 "192.168.1.114"  // Local mosquitto broker app on RPi CM5
 #define SECRET_MQTT_PORT "1883"
 #define SECRET_MQTT_TOPIC_PREFIX_SENSORS "sensors"
 #define SECRET_MQTT_TOPIC_PREFIX_LIGHTS "lights"
